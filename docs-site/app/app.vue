ab9243614534dd08edf6e2ac897e56a337c4bf40
Chris Schooley <chris@schooley.name>
2026-08-03 09:46:15 -0600

    chore: bump version to 2.11.0

--- docs-site/app/app.vue
+++ docs-site/app/app.vue
@@ -334,7 +334,7 @@ const { t, direction, initLocale, isLocaleLoaded } = useI18n();
 const baseURL = useRuntimeConfig().app.baseURL;
 const asset = (path: string) => `${baseURL}${path.replace(/^\/+/, '')}`;
 
-const version = ref('2.10.0');
+const version = ref('2.11.0');
 const contributors = ref<{ name: string; link: string }[]>([]);
 
 // Platform download cards. When we can detect the visitor's OS we surface only
